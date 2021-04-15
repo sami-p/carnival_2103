fab3a0d0e63aced1a69b8f78228460eb7e00d62f
sami-p <sami.peterson14@gmail.com>
2021-04-15 10:55:27 -0500

    Create add_ride method; pass add rides test

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,9 +1,13 @@
 class Carnival
   attr_reader :name,
-              :rides 
+              :rides
 
   def initialize(name)
     @name = name
     @rides = []
+  end
+
+  def add_ride(ride)
+    @rides << ride 
   end
 end
