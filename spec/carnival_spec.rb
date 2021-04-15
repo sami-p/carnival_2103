c9136996154c9290bd540c568894e5537ee6b6d7
sami-p <sami.peterson14@gmail.com>
2021-04-15 10:54:25 -0500

    Test if Carnival can add rides

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe Carnival do
     jeffco_fair = Carnival.new("Jefferson County Fair")
     expect(jeffco_fair.rides).to eq([])
   end
+
+  it 'can add rides' do
+    jeffco_fair = Carnival.new("Jefferson County Fair")
+    ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+    bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+    scrambler = Ride.new({name: 'Scrambler', cost: 15})
+
+    jeffco_fair.add_ride(ferris_wheel)
+    jeffco_fair.add_ride(bumper_cars)
+    jeffco_fair.add_ride(scrambler)
+
+    expect(jeffco_fair.rides).to eq([ferris_wheel, bumper_cars, scrambler])
+  end
 end
