3348961177939ec2b2f56964d953263f4c93e78a
sami-p <sami.peterson14@gmail.com>
2021-04-15 10:31:42 -0500

    Write 'starts with no interests' test

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -16,4 +16,9 @@ RSpec.describe Attendee do
     attendee = Attendee.new('Bob', 20)
     expect(attendee.spending_money).to eq(20)
   end
+
+  it 'starts with no interests' do
+    attendee = Attendee.new('Bob', 20)
+    expect(attendee.interests).to eq([])
+  end
 end
