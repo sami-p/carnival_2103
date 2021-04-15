0243aed02a7f441b963709ac4e9fde3fc38b4157
sami-p <sami.peterson14@gmail.com>
2021-04-15 10:29:19 -0500

    Instantialize Attendee with spending money; pass spending money test

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,7 +1,9 @@
 class Attendee
-  attr_reader :name
-  
-  def initialize(name, age)
+  attr_reader :name,
+              :spending_money
+
+  def initialize(name, spending_money)
     @name = name
+    @spending_money = spending_money
   end
 end
