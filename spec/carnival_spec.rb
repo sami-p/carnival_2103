55acb93f160d5263c5efea60f975f9f7038145de
sami-p <sami.peterson14@gmail.com>
2021-04-15 10:49:28 -0500

    Test that Carnival starts with no rides

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -13,4 +13,9 @@ RSpec.describe Carnival do
     jeffco_fair = Carnival.new("Jefferson County Fair")
     expect(jeffco_fair.name).to eq("Jefferson County Fair")
   end
+
+  it 'starts with no rides' do
+    jeffco_fair = Carnival.new("Jefferson County Fair")
+    expect(jeffco_fair.rides).to eq([])
+  end
 end
