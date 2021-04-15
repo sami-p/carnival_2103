0e96587d075bfa29f1397d207d8feb8db3fb580c
sami-p <sami.peterson14@gmail.com>
2021-04-15 10:44:54 -0500

    Fix RSpec.describe error

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -3,7 +3,7 @@ require './lib/carnival'
 require './lib/attendee'
 require './lib/ride'
 
-RSpec describe Carnival do
+RSpec.describe Carnival do
   it 'exist' do
     jeffco_fair = Carnival.new("Jefferson County Fair")
     expect(jeffco_fair).to be_an_instance_of(Carnival)
