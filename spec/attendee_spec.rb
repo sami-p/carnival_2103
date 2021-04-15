42586a618b1b1363a147f07d0284183b860d2aec
sami-p <sami.peterson14@gmail.com>
2021-04-15 10:36:08 -0500

    Test if attendee can add interests

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -21,4 +21,12 @@ RSpec.describe Attendee do
     attendee = Attendee.new('Bob', 20)
     expect(attendee.interests).to eq([])
   end
+
+  it 'can add interests' do
+    attendee = Attendee.new('Bob', 20)
+    attendee.add_interest('Bumper Cars')
+    attendee.add_interest('Ferris Wheel')
+
+    expect(attendee.interests).to eq(["Bumper Cars", "Ferris Wheel"])
+  end
 end
