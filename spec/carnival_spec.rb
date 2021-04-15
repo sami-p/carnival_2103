f8b4d9c1e7fcbaa88485e47575ee88ade46fc57c
sami-p <sami.peterson14@gmail.com>
2021-04-15 11:01:51 -0500

    Test that Carnival can recommend rides

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -31,4 +31,24 @@ RSpec.describe Carnival do
 
     expect(jeffco_fair.rides).to eq([ferris_wheel, bumper_cars, scrambler])
   end
+
+  it 'can recommend rides based on interests' do
+    jeffco_fair = Carnival.new("Jefferson County Fair")
+    ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+    bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+    scrambler = Ride.new({name: 'Scrambler', cost: 15})
+    bob = Attendee.new('Bob', 20)
+    sally = Attendee.new('Sally', 20)
+
+    jeffco_fair.add_ride(ferris_wheel)
+    jeffco_fair.add_ride(bumper_cars)
+    jeffco_fair.add_ride(scrambler)
+
+    bob.add_interest('Ferris Wheel')
+    bob.add_interest('Bumper Cars')
+    sally.add_interest('Scrambler')
+
+    expect(jeffco_fair.recommend_rides(bob)).to eq([ferris_wheel, bumper_cars])
+    expect(jeffco_fair.recommend_rides(sally)).to eq([scrambler])
+  end
 end
